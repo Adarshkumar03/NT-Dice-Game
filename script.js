93c03627a10e7c06b596267ac318f073d44ae461
Adarsh Kumar <adarshkumar245@gmail.com>
2025-03-03 22:17:04 +0530

    add player switch functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("Script is running!!");
 
 let player1Turn = true;
+
 let p1cScore = 0;
 let p1sScore = 0;
+
 let p2cScore = 0;
 let p2sScore = 0;
 
@@ -10,43 +12,81 @@ const playerOneContainer = document.getElementById("one");
 const playerTwoContainer = document.getElementById("two");
 
 const rollDiceButton1 = document.getElementById("p1-dice");
-const rollDiceButton2 = document.getElementById("dice-roll-p2");
+const rollDiceButton2 = document.getElementById("p2-dice");
 
-const saveButton1 = document.getElementById("save-p1");
-const saveButton2 = document.getElementById("save-p2");
+const saveButton1 = document.getElementById("p1-save");
+const saveButton2 = document.getElementById("p2-save");
 
-const p1cCount = document.getElementById("p1cCount");
-const p1sCount = document.getElementById("p1sCount");
+const p1cCount = document.getElementById("p1-cCount");
+const p1sCount = document.getElementById("p1-sCount");
 
-const p2cCount = document.getElementById("p2cCount");
-const p2sCount = document.getElementById("p2sCount");
+const p2cCount = document.getElementById("p2-cCount");
+const p2sCount = document.getElementById("p2-sCount");
+
+const p1Choice = document.getElementById("p1-choice");
+const p2Choice = document.getElementById("p2-choice");
+
+const skipButton1 = document.getElementById("p1-skip");
+const skipButton2 = document.getElementById("p2-skip");
 
 rollDiceButton1.addEventListener("click", () => {
-    console.log("Roll Dice Button 1 clicked!!");
     const randomNumber = Math.floor(Math.random() * 6) + 1;
     p1cScore = randomNumber == 1 ? 0 : p1cScore + randomNumber;
     p1cCount.textContent = p1cScore;
+    p1Choice.classList.remove("hidden");
+    rollDiceButton1.disabled = true;
 });
 
 rollDiceButton2.addEventListener("click", () => {
-    console.log("Roll Dice Button 2 clicked!!");
     const randomNumber = Math.floor(Math.random() * 6) + 1;
     p2cScore = randomNumber == 1 ? 0 : p2cScore + randomNumber;
     p2cCount.textContent = p2cScore;
+    p2Choice.classList.remove("hidden");
+    rollDiceButton2.disabled = true;
 });
 
 saveButton1.addEventListener("click", () => {
-    console.log("Save Button 1 clicked!!");
     p1sScore += p1cScore;
     p1sCount.textContent = p1sScore;
     p1cScore = 0;
     p1cCount.textContent = p1cScore;
+    switchTurn();
 });
 
 saveButton2.addEventListener("click", () => {
-    console.log("Save Button 2 clicked!!");
     p2sScore += p2cScore;
     p2sCount.textContent = p2sScore;
     p2cScore = 0;
     p2cCount.textContent = p2cScore;
+    switchTurn();
 });
+
+skipButton1.addEventListener("click", () => {
+    switchTurn();
+});
+
+skipButton2.addEventListener("click", () => {
+    switchTurn();
+});
+
+
+const switchTurn = () => {
+    player1Turn = !player1Turn;
+    if(player1Turn){
+        playerOneContainer.classList.remove("inactive");
+        playerTwoContainer.classList.add("inactive");
+        rollDiceButton1.disabled = false;
+        rollDiceButton2.disabled = true;
+    }else{
+        playerOneContainer.classList.add("inactive");
+        playerTwoContainer.classList.remove("inactive");
+        rollDiceButton1.disabled = true;
+        rollDiceButton2.disabled = false;   
+    }
+    p1Choice.classList.add("hidden");
+    p2Choice.classList.add("hidden");
+}
+
+playerTwoContainer.classList.add("inactive");
+p1Choice.classList.add("hidden");
+p2Choice.classList.add("hidden");
